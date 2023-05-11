3c934d79e1a759af949d42cb26e14786861ac813
Nando <nandocoric97@gmail.com>
2023-05-11 09:56:54 +0200

    Wired javascript with html and made simple calculator with basic arithemtic operations

--- javascript.js
+++ javascript.js
@@ -1,26 +1,21 @@
-let firstNumber = 3;
-let operator = '+';
-let secondNumber = 5;
-
-
 const add = (a,b) =>  a + b;
 
 const subtract = (a,b) => a - b;
 
 const multiply = (a,b) => a * b;
 
-const divide = (a, b) => b === 0 ? 0 : num1 / num2;
+const divide = (a, b) => b === 0 ? 0 : a / b;
 
-const operate = (operator, num1, num2) => {
+const operate = (operator, a, b) => {
     switch(operator) {
     case '+':
-        return add(num1, num2);
+        return add(a, b);
     case '-':
-        return subtract(num1, num2);
+        return subtract(a, b);
     case '*':
-        return multiply(num1, num2);
+        return multiply(a, b);
     case '/':
-        return divide(num1, num2);
+        return divide(a, b);
     default:
         return 0;
     }
@@ -29,7 +24,7 @@ const operate = (operator, num1, num2) => {
 const display = document.getElementById('calculator-display');
 const buttons = document.getElementById('calculator-buttons');
 let currentOperator = null;
-let currentValue = 0;
+let firstNumber = null;
 
 buttons.addEventListener('click', (event) => {
     const button = event.target;
@@ -37,7 +32,7 @@ buttons.addEventListener('click', (event) => {
         addToDisplay(button.innerText);
     } else if (button.classList.contains('operator')){
         addOperator(button.innerText);
-    } else if (button.id === clear) {
+    } else if (button.id === 'clear') {
         clearDisplay();
     } else if (button.id === 'equals') {
         calculate();
@@ -54,8 +49,20 @@ function addToDisplay(digit) {
 
 function addOperator(operator) {
     currentOperator = operator;
-    currentValue = parseFloat(display.value);
-    display.value = 0;
+    firstNumber = parseFloat(display.value);
+    display.value = '0';
 }
 
-function
+function clearDisplay () {
+    display.value = '0';
+    currentOperator = null;
+    firstNumber = null;
+}
+
+function calculate() {
+    const secondNumber = parseFloat(display.value);
+    let result = operate(currentOperator, firstNumber, secondNumber);
+    display.value = result;
+    currentOperator = null;
+    firstNumber = result;
+}
